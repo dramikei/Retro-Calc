98111af21fc6807b7acc6edfb6239fa3572c944f
Raghav Vashisht <raghav2011rv@gmail.com>
2017-05-12 14:01:11 +0530

    Final App

--- Retro Calc/ViewController.swift
+++ Retro Calc/ViewController.swift
@@ -7,19 +7,127 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var outputLabl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    var leftValString = ""
+    var rightValString = ""
+    var result = ""
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    
     override func viewDidLoad() {
+        outputLabl.text = "0"
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let btn = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: btn!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed (sender: UIButton) {
+        playBtnSound()
+        runningNumber += "\(sender.tag)"
+        outputLabl.text = runningNumber
+        
     }
+    
+    @IBAction func onDividePressed (sender: AnyObject) {
+        playBtnSound()
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed (sender: AnyObject) {
+        playBtnSound()
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed (sender: AnyObject) {
+        playBtnSound()
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed (sender: AnyObject) {
+        playBtnSound()
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualsPressed (sender: AnyObject) {
+        playBtnSound()
+        processOperation(operation: currentOperation)
+    }
+    
+    @IBAction func onClearPressed(_ sender: Any) {
+        playBtnSound()
+        runningNumber = ""
+        leftValString = ""
+        rightValString = ""
+        result = ""
+        currentOperation = Operation.Empty
+        outputLabl.text = "0"
 
-
+        
+    }
+    func playBtnSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
+    
+    
+    func processOperation(operation: Operation) {
+        if currentOperation != Operation.Empty {
+            if runningNumber != "" {
+                rightValString = runningNumber
+                runningNumber = ""
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValString)! * Double(rightValString)!)"
+                    
+                    
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValString)! / Double(rightValString)!)"
+                    
+                    
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValString)! - Double(rightValString)!)"
+                    
+                    
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValString)! + Double(rightValString)!)"
+                    
+                    
+                }
+                leftValString = result
+                outputLabl.text = result
+            }
+            
+            currentOperation = operation
+        } else {
+            //This is the first time an Operation is Pressed
+            leftValString = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
+    
 }
 
